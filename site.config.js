d1a704a7d721f30417dba6d3e5659b801b4f8665
HyeonjiJang <114472483+Hyeonz1@users.noreply.github.com>
2024-04-20 20:38:58 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -57,7 +57,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
